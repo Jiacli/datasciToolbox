963abe595cef469bc599ddb9b4bddaeca115506d
Jiachen Li <jiacli.jk@gmail.com>
2015-01-09 21:25:18 -0500

    Update featGen

--- Preprocessing/featEngine/src/featGen.java
+++ Preprocessing/featEngine/src/featGen.java
@@ -2,11 +2,11 @@
  * 
  */
 
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,21 +27,27 @@ public class featGen {
             return;
         }
         
-        System.out.println("input file: " + args[0]);
-        System.out.println("output feature file: " + args[0] + ".out");
-        
+        sampleRoutine(args[0]);
+
+    }
+
+    public static void sampleRoutine(String inputfile) throws IOException {
+
+        System.out.println("input file: " + inputfile);
+        System.out.println("output feature file: " + inputfile + ".out");
+
         BufferedReader br = new BufferedReader(new InputStreamReader(
-                new FileInputStream(args[0])));
-        BufferedWriter bw = new BufferedWriter(new FileWriter(args[0]
+                new FileInputStream(inputfile)));
+        BufferedWriter bw = new BufferedWriter(new FileWriter(inputfile
                 + ".feat.out"));
-        BufferedWriter bw_label = new BufferedWriter(new FileWriter(args[0]
+        BufferedWriter bw_label = new BufferedWriter(new FileWriter(inputfile
                 + ".label.out"));
-        
+
         // set feature type here, each sample will be stored in an list
         List<Double> featList = new ArrayList<>();
         String format = "ssv";
         int featDim = -1;
-        
+
         // load original feature file
         String line = null;
         int ctAll = 0, ctValid = 0;
@@ -50,54 +56,54 @@ public class featGen {
                 continue;
             }
             ctAll++;
-            
+
             // process one data sample
             line = line.trim();
             String[] feats = line.split(" ");
-            
+
             if (featDim == -1) {
                 // suppose the first sample is right, use its
-                // dimension as the required feature dimension 
+                // dimension as the required feature dimension
                 featDim = feats.length;
             }
-            
+
             // check the feature dimension
             if (feats.length != featDim) {
                 System.out.println("invalid sample: " + line);
                 continue;
             }
             ctValid++;
-            
+
             // add to featList for converting format
             for (int i = 1; i < feats.length; i++) {
                 featList.add(Double.valueOf(feats[i]));
             }
-            
+
             // write out feature & label to file
-            String sample = toFormatedStr(featList, format);
+            String sample = toFormatedString(featList, format);
             String label = (int) Float.parseFloat(feats[0]) + "\n";
             bw.write(sample);
             bw_label.write(label);
-            
+
             featList.clear();
         }
-        
+
         // clean up
         br.close();
         bw.close();
         bw_label.close();
-        
+
         // report
         System.out.println("Process Finised!");
         System.out.println("Number of lines: " + ctAll);
         System.out.println("Valid lines: " + ctValid + ", invalid lines: "
-                + (ctAll - ctValid));        
+                + (ctAll - ctValid));
     }
-    
+
     // To space separated value file
-    private static String toFormatedStr(List<Double> list, String format) {
+    private static String toFormatedString(List<Double> list, String format) {
         StringBuilder sb = new StringBuilder();
-        
+
         for (int i = 0; i < list.size() - 1; i++) {
             sb.append(list.get(i));
             if ("tsv".equals(format)) {
@@ -110,7 +116,7 @@ public class featGen {
         }
         sb.append(list.get(list.size() - 1));
         sb.append("\n");
-        
+
         return sb.toString();
     }
 
