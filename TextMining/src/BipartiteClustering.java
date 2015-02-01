2b61113089c9e24743f9cfd110cc0ccad3d50676
Jiachen Li <jiacli.jk@gmail.com>
2015-01-31 23:26:50 -0500

    Added Bipartite clustering

---
+++ TextMining/src/BipartiteClustering.java
@@ -0,0 +1,23 @@
+import org.la4j.matrix.sparse.CCSMatrix;
+import org.la4j.matrix.sparse.CRSMatrix;
+
+
+/**
+ * 
+ */
+
+/**
+ * Bipartite clustering Algorithms for Text Clustering
+ * @author JK
+ *
+ */
+public class BipartiteClustering {
+
+    CRSMatrix docMatrix = new CRSMatrix(100, 14063);
+    
+    
+    public void loadDocVector(String file) {
+        
+    }
+    
+}
